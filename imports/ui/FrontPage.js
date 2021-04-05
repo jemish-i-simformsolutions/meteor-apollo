27ec38a80058626786d468ffa0b67914a547a556
Jemish Italiya <italiyajemish99@gmail.com>
2021-04-05 09:41:55 +0530

    Revert "Revert "commmit at 5-4-2021""
    This reverts commit 10e08452d426e89a27ea5ec5545578f671875433.

--- imports/ui/FrontPage.js
+++ imports/ui/FrontPage.js
@@ -5,7 +5,6 @@ import { FindDonation } from "./Component/FindDonation";
 import { PersonalDonation } from "./Component/PersonalDonation";
 import { PostDonation } from "./Component/PostDonation";
 import LoginPage from "./LoginPage";
-import Player from "./Player";
 import { SignUpPage } from "./SignUpPage";
 import Navbar from "./Component/Navbar";
 import { Provider } from "react-redux";
@@ -14,38 +13,36 @@ import "./../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const FrontPage = () => {
   const store = ConfigureStore();
   return (
-    <div>
+    <div className="app">
       <BrowserRouter>
-        <div className="app">
-          <div className="navbar">
-            <Provider store={store}>
-              <Navbar />
-            </Provider>
-          </div>
-
-          <Switch>
-            {/* <Route path="/login"/> */}
-            <div className="container">
-              <Route path="/signup">
-                <Provider store={store}>
-                  <SignUpPage />
-                </Provider>
-              </Route>
-              <Route path="/login">
-                <Provider store={store}>
-                  <LoginPage />
-                </Provider>
-              </Route>
-
-              <Route path="/post_donation" component={PostDonation} />
-              <Route path="/personal_donations" component={PersonalDonation} />
-
-              <Route path="/all_donations" component={AllDonation} />
-
-              <Route path="/find_donation" component={FindDonation} />
-            </div>
-          </Switch>
+        <div className="navbar1 ">
+          <Provider store={store}>
+            <Navbar />
+          </Provider>
         </div>
+
+        <Switch>
+          {/* <Route path="/login"/> */}
+          <div className="container1">
+            <Route path="/signup">
+              <Provider store={store}>
+                <SignUpPage />
+              </Provider>
+            </Route>
+            <Route path="/login">
+              <Provider store={store}>
+                <LoginPage />
+              </Provider>
+            </Route>
+
+            <Route path="/post_donation" component={PostDonation} />
+            <Route path="/personal_donations" component={PersonalDonation} />
+
+            <Route path="/all_donations" component={AllDonation} />
+
+            <Route path="/find_donation" component={FindDonation} />
+          </div>
+        </Switch>
       </BrowserRouter>
     </div>
   );
